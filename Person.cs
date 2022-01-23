76f17c43bb3c93218ed123977225bf706a06554f
Juan David Pardo <98267802+jpardopro@users.noreply.github.com>
2022-01-23 11:36:39 -0500

    Feature a (#1)
    * jpardo: creación clase Person
    
    * jpardo: creación clase Product

---
+++ Person.cs
@@ -0,0 +1,5 @@
+public class Person
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+}
---
+++ Product.cs
@@ -0,0 +1,4 @@
+public class Product
+{
+    public decimal Amount { get; set; }
+}
