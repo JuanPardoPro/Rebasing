670e9156cbb71bbd8e2a5daf9b46de41274e993a
Juan David Pardo <jhondeivid12@hotmail.com>
2022-01-24 11:43:55 -0500

    * Dominio
    * Se define el tipo explicito.

--- Customer.cs
+++ Customer.cs
@@ -2,4 +2,11 @@ public class Customer
 {
     public int Id { get; set; }
     public string InternalCode { get; set; }
+    public decimal SalaryByDay { get; set; }
+    public int DayWork { get; set; }
+
+    public decimal CalcularSalario()
+    {
+        return SalaryByDay * DayWork;
+    }
 }
